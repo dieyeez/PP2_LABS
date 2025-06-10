73129333a76de845b74b158fbd82a896e4441a75
Yerzhigit Islamkhan <erkow.islamxan@gmail.com>
2025-06-10 05:56:39 +0500

    Add files via upload

---
+++ lab_3/classes_1ex.py
@@ -0,0 +1,11 @@
+class String:
+    def getstring(self):
+        self.sentence = input("Sentense:")
+
+    def printstring(self):
+        print("With upper case:"+self.sentence.upper())
+
+
+mystring = String()
+mystring.getstring()
+mystring.printstring()
---
+++ lab_3/func2_12.py
@@ -0,0 +1,9 @@
+def palindrom(text):
+    if text == text[::-1]:
+        print("YES")
+    else:
+        print("NO")
+
+
+text = input("text: ")
+palindrom(text)
